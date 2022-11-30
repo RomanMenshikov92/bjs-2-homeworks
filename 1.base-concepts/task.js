79eb968c27317a037a2a6040696ce75b8ff45001
Roman <R-Tupac@yandex.ru>
2022-11-30 09:10:55 +0300

    added homework-1. Correction v.1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,13 +3,12 @@
 function solveEquation(a, b, c) {
   let arr = []; // объявление массива
   let discriminant = Math.pow(b, 2) - 4 * a * c; // объявления дискриминанта и присвооение формулы
-  // условие 1 если дискриминант больше 0, то два корня в рните результат из фумассив
+  // условие 1 если дискриминант больше 0, то два корня верните результат из массива
   // условие 2 если дискриминант меньше 0, то ничего не делаем
   // условие 3 если дискриминант равен 0, то 1 корень в массив
   if (discriminant > 0) {
     arr[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
     arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
-  } else if (discriminant < 0) {
   } else if (discriminant === 0) {
     arr[0] = -b / (2 * a);
   }
@@ -18,15 +17,15 @@ function solveEquation(a, b, c) {
 
 // функция для решения дискриминанта с тремя параметрами a,b,c
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  // проверка значения isNaN
-  if (isNaN(ParseInt(percent)) === true || ParseInt(percent) < 0) {
-    return 'Неверно введен первый параметр';
-  } else if (isNaN(ParseInt(contribution)) === true || ParseInt(contribution) < 0) {
-    return 'Неверно введен второй параметр';
-  } else if (isNaN(ParseInt(amount)) === true || ParseInt(amount) < 0) {
-    return 'Неверно введен третий параметр';
-  } else if (isNaN(ParseInt(countMonths)) === true || ParseInt(countMonths) < 0) {
-    return 'Неверно введен четвертый параметр';
+  // проверка значения isNaN и меньше 0
+  if (isNaN(Number.parseInt(percent)) === true || Number.parseInt(percent) < 0) {
+    return false;
+  } else if (isNaN(Number.parseInt(contribution)) === true || Number.parseInt(contribution) < 0) {
+    return false;
+  } else if (isNaN(Number.parseInt(amount)) === true || Number.parseInt(amount) < 0) {
+    return false;
+  } else if (isNaN(Number.parseInt(countMonths)) === true || Number.parseInt(countMonths) < 0) {
+    return false;
   }
 
   // преобразование процентной ставки от 0 до 100 из диапазоне от 0 до 1 и из готовой ставки в месячную
@@ -34,11 +33,11 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   // тело кредита = сумма кредита - первоначальный взнос
   let loanBody = amount - contribution;
   // ежемесячная оплата с формулой
-  monthlyPayment = loanBody * (interestRate + (interestRate / (((1 + interestRate) ** countMonths) - 1)));
+  let monthlyPayment = loanBody * (interestRate + (interestRate / (((1 + interestRate) ** countMonths) - 1)));
   // общая сумма = ежемесячная оплата * период
-  totalAmount = monthlyPayment * countMonths;
+  let totalAmount = monthlyPayment * countMonths;
   // возвращаем результат до двух значений после запятой. Результат должен быть числом
-  return ParseInt(totalAmount.toFixed(2));
+  return Number.parseFloat(totalAmount.toFixed(2));
 }
 
 
